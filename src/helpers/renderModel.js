644ec07be15bbf9d6f434e9cf6d3c930adc836b4
PdoubleU <piotrwitasik1988@gmail.com>
2021-06-15 10:24:26 +0200

    [fix 3d model]: 	- fix relative path 	- fix camera distance/angle 	- fix object scale properties [set up threejs]: 	- set up light 	- set up camera 	- set up rotation of object

--- src/helpers/renderModel.js
+++ src/helpers/renderModel.js
@@ -3,23 +3,24 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const renderModel = () => {
   const canvas = document.querySelector('.model')
+  var model
 
   const scene = new THREE.Scene()
 
   {
-    const color = 0xFFFFFF
-    const intensity = 1
+    const color = 0xADBAC9
+    const intensity = 5
     const light = new THREE.DirectionalLight(color, intensity)
-    light.position.set(-1, 2, 4)
+    light.position.set(90, 35, 10)
     scene.add(light)
   }
 
   const fov = 75
   const aspect = canvas.clientWidth / canvas.clientHeight
   const near = 0.1
-  const far = 5
+  const far = 1000
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  camera.position.z = 2
+  camera.position.z = 10
 
   const renderer = new THREE.WebGLRenderer({ canvas, alpha: true })
   renderer.setSize(canvas.clientWidth, canvas.clientHeight)
@@ -27,17 +28,21 @@ const renderModel = () => {
   document.body.appendChild(renderer.domElement)
 
   const gltfLoader = new GLTFLoader()
-  const url = '/scene.gltf'
+  const url = '/death_star/scene.gltf'
   gltfLoader.load(url, (gltf) => {
-    console.log(gltf)
-    const root = gltf.scene
-    scene.add(root)
-    console.log(scene)
+    gltf.scene.scale.set(0.05, 0.05, 0.05)
+    model = gltf.scene
+    model.rotation.x += 0.6
+    scene.add(model)
   }
   )
 
+  camera.position.z = 100
+
   function animate () {
     requestAnimationFrame(animate)
+
+    if (model) model.rotation.y -= 0.01
     renderer.render(scene, camera)
   }
   animate()
