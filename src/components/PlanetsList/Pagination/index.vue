dcb943c00f6a04417a4bb5428e1fc7034c89b4ed
PdoubleU <piotrwitasik1988@gmail.com>
2021-06-18 18:37:48 +0200

    [fix_deploy]

--- src/components/PlanetsList/Pagination/index.vue
+++ src/components/PlanetsList/Pagination/index.vue
@@ -3,7 +3,7 @@
       <el-pagination
       layout="prev, pager, next"
       :total="total"
-      v-if="!loading && !error"
+      :class="[!loading && !error ? '' : 'hidden']"
       @current-change="fetchData"
       @prev-click="fetchData"
       @next-click="fetchData">
